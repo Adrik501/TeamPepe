d1df277ddd91ab01d668553e69d71663c57235fa
Adrian <a.soluk11@gmail.com>
2024-04-29 12:39:42 +0200

    new code

---
+++ app/Adrians Code zum testen/main.js
@@ -0,0 +1,38 @@
+// Import the required functions and modules
+import { IPConnection } from './assets/content';
+import { setup, temperatureHandler, rgbButtonPressed, motionDetectorHandler, humidityHandler, dualButtonPressedHandler } from './functions';
+
+// Establish connection
+const ipcon = new IPConnection();
+ipcon.connect(HOST, PORT);
+
+// Instantiate all sensors
+const temperatureBricklet = new BrickletPTCV2(TEMP, ipcon);
+const ambientLight = new BrickletAmbientLightV3(LIGH, ipcon);
+const humidityBricklet = new BrickletHumidityV2(HUMI, ipcon);
+const motionDetector = new BrickletMotionDetectorV2(MOTI, ipcon);
+const rgbButton = new BrickletRGBLEDButton(RGBB, ipcon);
+const dualButton = new BrickletDualButtonV2(DBTN, ipcon);
+
+// Setup some settings
+setup();
+
+// Setup callbacks for all sensors
+temperatureBricklet.registerCallback(temperatureBricklet.CALLBACK_TEMPERATURE, temperatureHandler);
+temperatureBricklet.setTemperatureCallbackConfiguration(5000, false, "x", 0, 0); // updates temperature every 5 seconds
+
+rgbButton.registerCallback(rgbButton.CALLBACK_BUTTON_STATE_CHANGED, rgbButtonPressed);
+
+motionDetector.registerCallback(motionDetector.CALLBACK_MOTION_DETECTED, motionDetectorHandler);
+
+humidityBricklet.registerCallback(humidityBricklet.CALLBACK_HUMIDITY, humidityHandler);
+humidityBricklet.setHumidityCallbackConfiguration(5000, false, "x", 0, 0); // updates humidity every 5 seconds
+
+dualButton.registerCallback(dualButton.CALLBACK_STATE_CHANGED, dualButtonPressedHandler);
+
+// Program stays open and waits for callbacks to handle until it is exited
+console.log('Press any button to exit the program...');
+process.stdin.once('data', () => {
+    console.log('Program exited!');
+    ipcon.disconnect();
+});
